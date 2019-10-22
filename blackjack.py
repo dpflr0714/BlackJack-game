a58a1c3187a18bde34323e6b2f8439031c7a5916
dpflr0714 <jun.park@amwestfunding.com>
2019-10-21 17:10:17 -0700

    Changes

--- blackjack.py
+++ blackjack.py
@@ -14,40 +14,44 @@ for x in range(0,2):
 dealercalculate = sum(dealershand)
 yourcalculate = sum(yourhand)
 
-if dealercalculate == 21:
-    print("Dealer got blackjack")
 
-while dealercalculate < 18:
+
+def dealerplay():
+if 17 < dealercalculate < 21:
+    i = 1
+elif dealercalculate < 17:
+    while dealercalculate < 17:
         dealershand.append(random.randrange(2,12,1))
+        dealercalculate = sum(dealershand)
         for i, x in enumerate(dealershand):
             if x == 11:
                 dealershand[i] = 1
                 dealercalculate = sum(dealershand)
+                i = 1
 
+elif dealercalculate <
+else:
+    print("***Dealer got 21***")
+    i = 0
 
-
-
-print("Here are you cards: ")
+print("Here are your cards: ")
 print(yourhand)
 
 print("Here are the dealers card:")
 print(dealershand)
 
 
-i = 1
 while i == 1:
-    if dealercalculate < 21 and yourcalculate < 21:
+    if yourcalculate < 21:
         print("Hit or Stand? Y or N")
         user_choice = input()
-    if user_choice == 'y':
-        yourhand.append(random.randrange(2, 12, 1))
-        print(yourhand)
-        if yourcalculate <= 21:
-            i = 1
-        else:
-            print("You busted your ass son")
-    elif user_choice == 'n':
-        break
+        if user_choice == 'y':
+            yourhand.append(random.randrange(2, 12, 1))
+            print(yourhand)
+        elif user_choice == 'n':
+            break
+    elif yourcalculate > 21:
+        print("You busted your ass")
     else:
         print("please press y or n")
         continue
@@ -60,13 +64,22 @@ while i == 1:
 dealercalculate = sum(dealershand)
 yourcalculate = sum(yourhand)
 
-if dealercalculate > yourcalculate:
-   print("You suck ass")
-else:
-    if dealercalculate < yourcalculate:
-        print("Noice")
-    else:
-        print("Draw, play again")
+
+# Need to check for 11 on players' deck and give a choice of 11 or 1
+# If dealer goes over 21, it becomes a bust
+# The player wins as long as its greater than dealer regardless of going over 21, need to fix
+
+
+
+
+
+
+
+
+
+
+
+
 
 #if dealershand
 
